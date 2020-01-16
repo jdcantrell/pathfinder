7145d096d40e5a9b058ecdc8648f9918b378f3c7
JD Cantrell <jd@goodrobot.net>
2020-01-16 00:03:31 -0800

    Add a temp placeholder

--- pathfinder.py
+++ pathfinder.py
@@ -1,6 +1,29 @@
 import os
+from datetime import datetime
 
-os.mkdir("./build")
+if not os.path.isdir("./build"):
+    os.mkdir("./build")
+
+this_year = 2020
+start = datetime(this_year, 1, 1)
+end = datetime(this_year + 1, 1, 1)
+now = datetime.now()
+
+total = end - start
+current = now - start
+
+percent = round(current / total * 100)
+blocks = round(current / total * 80)
+
+run = round(19.7 / 600 * 100)
+run_blocks = round(19.7 / 600 * 80)
 
 with open("./build/index.html", "w") as f:
-    f.write("<h1>Hello</h1>")
+    f.write("<h1>You got this!</h1>")
+    f.write("<pre>")
+    f.write("Year: {}%\n".format(percent))
+    f.write("{}{}\n".format("▓" * blocks, "░" * (80 - blocks)))
+    f.write("\n")
+    f.write("Running Distance: {}%\n".format(run))
+    f.write("{}{}\n".format("▓" * run_blocks, "░" * (80 - run_blocks)))
+    f.write("</pre>")
